a46fabf053c700d59708195473afda7f5715d6d9
GeethalWickramasingha <e19436@eng.pdn.ac.lk>
2025-07-20 10:53:04 +0530

    readme technical added

--- src/uspacehelper/volcom_agents/employer/volcom_employer.c
+++ src/uspacehelper/volcom_agents/employer/volcom_employer.c
@@ -27,8 +27,8 @@
 #define BUFFER_SIZE 2048
 #define STALE_THRESHOLD 15
 #define MAX_EMPLOYEES 100
-#define CHUNKED_SET_PATH "/home/dasun/Projects/e19-4yp-Voluntary-Computing-Operating-System/src/uspacehelper/scripts"
-#define RESULTS_PATH "/home/dasun/Projects/e19-4yp-Voluntary-Computing-Operating-System/src/uspacehelper/results"
+#define CHUNKED_SET_PATH "/home/geeth99/Desktop/chuncked_set"
+#define RESULTS_PATH "//home/geeth99/Desktop/chuncked_set/results"
 #define MAX_CHUNKS 1024
 #define MAX_FILENAME_LEN 256
 #define EMPLOYEE_PORT 12345
